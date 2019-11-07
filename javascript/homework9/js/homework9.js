ef609f2dbdc8e4b7acf1d78368940a424ebe2d32
shehlaibadzadeh2019 <56446878+shehlaibadzadeh2019@users.noreply.github.com>
2019-11-07 06:13:27 +0400

    Update homework9.js

--- javascript/homework9/js/homework9.js
+++ javascript/homework9/js/homework9.js
@@ -1,9 +1,20 @@
-$('.tabs-title').not('.active').click(function(){
-    let index = $(this).index();
-    let content = $('.tabs-content-item').eq(index);
-    $(this).addClass('active').siblings().removeClass('active');
-    $('.tabs-content-item').removeClass('active').eq(index).addClass('active');
-});
-
-$('.tabs-title:first').addClass('active');
-$('.tabs-content-item:first').addClass('active');
+function openCity(evt, cityName) {
+    // Declare all variables
+    let i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabs-content-item");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tabs-title");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
