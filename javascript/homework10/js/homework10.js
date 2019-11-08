e828b69c08c585d8a2a53011ef5ae7b424f60464
shehlaibadzadeh2019 <56446878+shehlaibadzadeh2019@users.noreply.github.com>
2019-11-08 21:10:16 +0400

    Add files via upload

---
+++ javascript/homework10/js/homework10.js
@@ -0,0 +1,21 @@
+document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('icon-password')) {
+        const input = event.target.previousElementSibling;
+        if (input.type === 'password') {
+            input.type = 'text';
+
+        } else {
+            input.type = 'password'
+        }
+        event.target.classList.toggle('fa-eye-slash');
+        console.log(input);
+    }
+    if (event.target.classList.contains('btn')) {
+        event.preventDefault();
+        if (document.querySelectorAll('input')[0].value===document.querySelectorAll('input')[1].value) {
+            alert('You are welcome');
+        } else {
+            document.getElementById('red-text').style.display = 'block'
+        }
+    }
+});
