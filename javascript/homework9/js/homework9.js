2ebf905c780f33e4694a29ba2c190dfb96713035
shehlaibadzadeh2019 <56446878+shehlaibadzadeh2019@users.noreply.github.com>
2019-11-10 14:54:10 +0400

    Update homework9.js

--- javascript/homework9/js/homework9.js
+++ javascript/homework9/js/homework9.js
@@ -1,20 +1,17 @@
-function openCity(evt, cityName) {
-    // Declare all variables
-    let i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabs-content-item");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tabs-title");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
+let tabs = document.getElementById('tabs-link');
+let tabsContent = document.getElementById('tabs-content');
+
+
+tabs.onclick = (e) => {
+	tabs.querySelector(".active").classList.toggle("active");
+	tabsContent.querySelector("li:not([hidden])").hidden = true;
+	e.target.classList.toggle("active");
+	tabsContent.children[e.target.dataset.index].hidden = false;
+}
+ 
+
+for (let i = 0; i < tabsContent.children.length; i++){
+	tabs.children[i].dataset.index = i;
+	if(i)
+    tabsContent.children[i].hidden = true;
+}
