6c7f97ceb6de392eff481a10279787291c83e63e
shehlaibadzadeh2019 <56446878+shehlaibadzadeh2019@users.noreply.github.com>
2019-11-11 20:17:21 +0400

    Update homework11.js

--- javascript/homework11/js/homework11.js
+++ javascript/homework11/js/homework11.js
@@ -1,53 +1,15 @@
-document.addEventListener('keydown', function(e) {
-    const btn = document.getElementsByClassName("btn");
-    switch (e.code){
-        case "Enter":
-            btn[0].style.backgroundColor = "blue";
-            break;
-        case "KeyS":
-            btn[1].style.backgroundColor = "blue";
-            break;
-        case "KeyE":
-            btn[2].style.backgroundColor = "blue";
-            break;
-        case "KeyO":
-            btn[3].style.backgroundColor = "blue";
-            break;
-        case "KeyN":
-            btn[4].style.backgroundColor = "blue";
-            break;
-        case "KeyL":
-            btn[5].style.backgroundColor = "blue";
-            break;
-        case "KeyZ":
-            btn[6].style.backgroundColor = "blue";
-            break;
-    }
-})
+let allButtons = document.querySelectorAll(".btn");
 
-document.addEventListener('keyup', function(e) {
-    const btn = document.getElementsByClassName("btn");
-    switch (e.code){
-        case "Enter":
-            btn[0].style.backgroundColor = "#33333a";
-            break;
-        case "KeyS":
-            btn[1].style.backgroundColor = "#33333a";
-            break;
-        case "KeyE":
-            btn[2].style.backgroundColor = "#33333a";
-            break;
-        case "KeyO":
-            btn[3].style.backgroundColor = "#33333a";
-            break;
-        case "KeyN":
-            btn[4].style.backgroundColor = "#33333a";
-            break;
-        case "KeyL":
-            btn[5].style.backgroundColor = "#33333a";
-            break;
-        case "KeyZ":
-            btn[6].style.backgroundColor = "#33333a";
-            break;
-    }
-})
+document.body.addEventListener('keydown', () => {
+    allButtons.forEach((element)=> {
+        element.style.backgroundColor = "black";
+    })
+});
+
+document.body.addEventListener('keyup', (event) => {
+    allButtons.forEach((element) => {
+        if (element.innerText === event.key.toUpperCase()) {
+            element.style.backgroundColor = "blue";
+        }
+    });
+});
