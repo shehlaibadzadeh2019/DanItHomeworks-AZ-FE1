3c5259d800c98f638f22ecdbdb0d92367a07e97f
shehlaibadzadeh2019 <56446878+shehlaibadzadeh2019@users.noreply.github.com>
2019-11-11 20:05:15 +0400

    Update homework11.js

--- javascript/homework11/js/homework11.js
+++ javascript/homework11/js/homework11.js
@@ -1,50 +1,53 @@
 document.addEventListener('keydown', function(e) {
     const btn = document.getElementsByClassName("btn");
-    if (e.code == "Enter") {
-        btn[0].style.backgroundColor = "blue"
-    }
-    if (e.code == "KeyS") {
-        btn[1].style.backgroundColor = "blue"
-    }
-    if (e.code == "KeyE") {
-        btn[2].style.backgroundColor = "blue"
-    }
-    if (e.code == "KeyO") {
-        btn[3].style.backgroundColor = "blue"
-    }
-    if (e.code == "KeyN") {
-        btn[4].style.backgroundColor = "blue"
-    }
-    if (e.code == "KeyL") {
-        btn[5].style.backgroundColor = "blue"
-    }
-    if (e.code == "KeyZ") {
-        btn[6].style.backgroundColor = "blue"
+    switch (e.code){
+        case "Enter":
+            btn[0].style.backgroundColor = "blue";
+            break;
+        case "KeyS":
+            btn[1].style.backgroundColor = "blue";
+            break;
+        case "KeyE":
+            btn[2].style.backgroundColor = "blue";
+            break;
+        case "KeyO":
+            btn[3].style.backgroundColor = "blue";
+            break;
+        case "KeyN":
+            btn[4].style.backgroundColor = "blue";
+            break;
+        case "KeyL":
+            btn[5].style.backgroundColor = "blue";
+            break;
+        case "KeyZ":
+            btn[6].style.backgroundColor = "blue";
+            break;
     }
 })
 
 document.addEventListener('keyup', function(e) {
     const btn = document.getElementsByClassName("btn");
-    if (e.code == "Enter") {
-        btn[0].style.backgroundColor = "#33333a"
-    }
-    if (e.code == "KeyS") {
-        btn[1].style.backgroundColor = "#33333a"
-    }
-    if (e.code == "KeyE") {
-        btn[2].style.backgroundColor = "#33333a"
-    }
-    if (e.code == "KeyO") {
-        btn[3].style.backgroundColor = "#33333a"
-    }
-    if (e.code == "KeyN") {
-        btn[4].style.backgroundColor = "#33333a"
-    }
-    if (e.code == "KeyL") {
-        btn[5].style.backgroundColor = "#33333a"
-    }
-
-    if (e.code == "KeyZ") {
-        btn[6].style.backgroundColor = "#33333a"
+    switch (e.code){
+        case "Enter":
+            btn[0].style.backgroundColor = "#33333a";
+            break;
+        case "KeyS":
+            btn[1].style.backgroundColor = "#33333a";
+            break;
+        case "KeyE":
+            btn[2].style.backgroundColor = "#33333a";
+            break;
+        case "KeyO":
+            btn[3].style.backgroundColor = "#33333a";
+            break;
+        case "KeyN":
+            btn[4].style.backgroundColor = "#33333a";
+            break;
+        case "KeyL":
+            btn[5].style.backgroundColor = "#33333a";
+            break;
+        case "KeyZ":
+            btn[6].style.backgroundColor = "#33333a";
+            break;
     }
 })
