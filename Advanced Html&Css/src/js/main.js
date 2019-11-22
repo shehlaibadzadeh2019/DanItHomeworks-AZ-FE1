153ac5bf84d331bffb5afdc9958b144bf8f232c2
shehlaibadzadeh2019 <56446878+shehlaibadzadeh2019@users.noreply.github.com>
2019-11-23 00:09:51 +0400

    Add files via upload

---
+++ Advanced Html&Css/src/js/main.js
@@ -0,0 +1,20 @@
+let mobaleMenuIsOpen = false;
+const openBtn = document.querySelector('.btn-menu');
+const openMenu = document.querySelector('.navbar-toggler');
+const closeMenu = document.querySelector('.submenu-btn');
+const menu = document.querySelector('.submenu');
+
+openMenu.addEventListener('click', ()=> {
+    if (!mobaleMenuIsOpen) {
+        mobaleMenuIsOpen = true;
+        menu.className = 'submenu open';
+        openBtn.classList = 'btn-menu close';
+        closeMenu.classList = 'submenu-btn open'
+    } else {
+        mobaleMenuIsOpen = false;
+        menu.className = 'submenu';
+        openBtn.classList = 'btn-menu';
+        closeMenu.classList = 'submenu-btn';
+    }
+});
+
